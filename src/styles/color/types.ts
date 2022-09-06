449214648e267406999d2cdf0fd4095c5802aa79
formegusto <formegusto@gmail.com>
2022-09-06 13:12:09 +0900

    color set ready

--- src/styles/color/types.ts
+++ src/styles/color/types.ts
@@ -1,3 +1,14 @@
-export interface Palette {
-  [key: number]: string;
-}
+export type PaletteKey =
+  | 50
+  | 100
+  | 200
+  | 300
+  | 400
+  | 500
+  | 600
+  | 700
+  | 800
+  | 900;
+export type Palette = {
+  [key in PaletteKey]?: string;
+};
