17e5b297031262689e305e086c48253aae4eb7d6
formegusto <formegusto@gmail.com>
2022-09-07 17:08:06 +0900

    Symbol Mail

--- src/pages/main/index.tsx
+++ src/pages/main/index.tsx
@@ -1,3 +1,18 @@
+import { Mail3D } from "@asset/symbols";
+import styled from "styled-components";
+
 export function MainPage() {
-  return <>메인페이지</>;
+  return (
+    <Screen>
+      <Mail3D />
+    </Screen>
+  );
 }
+
+const Screen = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  height: 1500px;
+`;
